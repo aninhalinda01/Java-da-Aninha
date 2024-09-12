4c2fb011199fed29a1ee4a97cb49052e8ae248df
aninhalinda01 <161717633+aninhalinda01@users.noreply.github.com>
2024-09-12 08:49:15 -0300

    Add files via upload

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,5 +1,12 @@
 console.log("Meu Primeiro Programa: trabalhando com Variáveis");
 
-const idade = 26;
+const idade = 16;
 
 console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
