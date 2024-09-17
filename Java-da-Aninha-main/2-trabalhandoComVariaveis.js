8ed9fa67efdc0052adfb7bb06ae35cd9790b67df
aninhalinda01 <161717633+aninhalinda01@users.noreply.github.com>
2024-09-17 08:52:03 -0300

    Add files via upload

---
+++ Java-da-Aninha-main/2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//console.log("Trabalhando com variáveis");
+console.log("trabalhando com varoáveis");
+//JS é case sensitive
+
+idade = 16;
+const nome = "Ana";
+let ano = 2024;
+
+//não fazer 
+//mes = "Janeiro";
